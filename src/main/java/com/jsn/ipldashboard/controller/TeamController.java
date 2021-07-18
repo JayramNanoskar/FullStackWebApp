fe1de543a9b2e6d8964480872c14a63eafd98420
JayramNanoskar <jayram.n978@gmail.com>
2021-07-19 00:35:08 +0530

    Correcting typo & indentation

--- src/main/java/com/jsn/ipldashboard/controller/TeamController.java
+++ src/main/java/com/jsn/ipldashboard/controller/TeamController.java
@@ -4,7 +4,6 @@ import com.jsn.ipldashboard.model.Team;
 import com.jsn.ipldashboard.repository.MatchRepository;
 import com.jsn.ipldashboard.repository.TeamRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +14,10 @@ public class TeamController {
     private TeamRepository teamRepository;
     private MatchRepository matchRepository;
 
-    @Autowired
     public TeamController(TeamRepository teamRepository, MatchRepository matchRepository) { //This is going to Autowired mentioned Repositories. This is constructor injection but still @Autowired not needed because presence of only constructor.
         this.teamRepository = teamRepository;
         this.matchRepository = matchRepository;
     }
-
-    public TeamController(){}
 
     @GetMapping("/team/{teamName}")
     public Team getTeam(@PathVariable String teamName){
