676d527ffa91dbee4b8c72bd0eca399a33fe4034
JayramNanoskar <jayram.n978@gmail.com>
2021-07-18 21:17:23 +0530

    Preparing data using JPQL

--- src/main/java/com/jsn/ipldashboard/data/JobCompletionNotificationListener.java
+++ src/main/java/com/jsn/ipldashboard/data/JobCompletionNotificationListener.java
@@ -1,5 +1,12 @@
 package com.jsn.ipldashboard.data;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+
+import com.jsn.ipldashboard.model.Team;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
@@ -8,6 +15,7 @@ import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
@@ -16,19 +24,45 @@ public class JobCompletionNotificationListener extends JobExecutionListenerSuppo
 
   private final JdbcTemplate jdbcTemplate;
 
+  private final EntityManager entityManager; // It is JPA way of interacting with database
+
   @Autowired
-  public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
+  public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate, EntityManager entityManager) {
     this.jdbcTemplate = jdbcTemplate;
+    this.entityManager = entityManager;
   }
 
   @Override
+  @Transactional
   public void afterJob(JobExecution jobExecution) {
-    if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
+    if(jobExecution.getStatus() == BatchStatus.COMPLETED) { //Runs when job is complete
       log.info("!!! JOB FINISHED! Time to verify the results");
+      
+      Map<String, Team> teamData = new HashMap<>();
+      entityManager.createQuery("select distinct m.team1, count(*) from Match m group by m.team1", Object[].class) //Getting Object array having team name as String & count of matches as Number
+      .getResultList()
+      .stream()
+      .map(e -> new Team((String)e[0], (long)e[1]))  //Creating team instances
+      .forEach(team -> teamData.put(team.getTeamName(), team));
+      
+      entityManager.createQuery("select distinct m.team2, count(*) from Match m group by m.team2", Object[].class) 
+      .getResultList()
+      .stream()
+      .forEach(e -> {
+        Team team = teamData.get((String)e[0]);
+        team.setTotalMatches(team.getTotalMatches() + (long)e[1]);
+      });
 
-      jdbcTemplate.query("SELECT team1, team2, date FROM match",
-        (rs, row) -> "Team 1 "+ rs.getString(1)+ " Team 2 "+ rs.getString(2)+ " Date "+ rs.getString(3)
-      ).forEach(str -> System.out.println(str));
+      entityManager.createQuery("select m.matchWinner, count(*) from Match m group by m.matchWinner", Object[].class)
+      .getResultList()
+      .stream()
+      .forEach(e -> {
+        Team team = teamData.get((String)e[0]);
+        if(team != null) team.setTotalWins((long)e[1]);
+      });
+
+      teamData.values().forEach(team -> entityManager.persist(team));
+      teamData.values().forEach(team -> System.out.println(team));
     }
   }
 }
