3714b9c6c8809f423d85f8374b564e15b463bf22
JayramNanoskar <jayram.n978@gmail.com>
2021-07-22 19:47:20 +0530

    Styling the Team page

--- src/frontend/src/components/MatchSmallCard.js
+++ src/frontend/src/components/MatchSmallCard.js
@@ -1,16 +1,20 @@
 import { React } from 'react';
 import { Link } from 'react-router-dom';
+import './MatchSmallCard.scss';
 
 export const MatchSmallCard = ({teamName, match }) => { //Accepting a parameters & making Object Destructuring
     if (!match) return null; //Telling if parameter match is null then don't do anything
 
     const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
     const otherTeamRoute = `/teams/${otherTeam}`;
+    const isMatchWon = teamName === match.matchWinner;
 
     return (
-        <div className="MatchSmallCard">
-            <h3>vs <Link to={otherTeamRoute}>{otherTeam}</Link></h3>
-            <p>{match.matchWinner} won by {match.resultMargin} {match.result}</p>
+        <div className={isMatchWon ? 'MatchSmallCard won-card' : 'MatchSmallCard lost-card'}>
+        <span className="vs">vs</span>
+        <h1><Link to={otherTeamRoute}>{otherTeam}</Link></h1>
+        <p className="match-result">{match.matchWinner} won by {match.resultMargin} {match.result}</p>
+
         </div>
     );
 }
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -2,11 +2,14 @@ import { React, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
+import { PieChart } from 'react-minimal-pie-chart';
+
+import './TeamPage.scss';
 
 export const TeamPage = () => {
 
   //The state is just a fancy term for a JavaScript data structure.
-  const [team, setTeam] = useState({matches: []}); //Initializing state in this component such as defining matches as an empty array & specifying state name i.e. team & function to set state i.e. setTeam
+  const [team, setTeam] = useState({ matches: [] }); //Initializing state in this component such as defining matches as an empty array & specifying state name i.e. team & function to set state i.e. setTeam
   const { teamName } = useParams(); //Giving an Object with all of the PathParams & Destructuring to get teamName
 
 
@@ -24,16 +27,33 @@ export const TeamPage = () => {
   );
 
 
-  if(!team || !team.teamName){
+  if (!team || !team.teamName) {
     return <h1>Team not found</h1>
   }
 
   //JSX allows us to write HTML elements in JavaScript and place them in the DOM without any createElement() and/or appendChild() methods. JSX converts HTML tags into react elements. We are not required to use JSX, but JSX makes it easier to write React applications.
   return (
     <div className="TeamPage">
-      <h1>{team.teamName}</h1>
-      <MatchDetailCard teamName={team.teamName} match={team.matches[0]}></MatchDetailCard>
+      <div className="team-name-section">
+        <h1 className="team-name">{team.teamName}</h1>
+      </div>
+      <div className="win-loss-section">
+        Wins / Losses
+        <PieChart
+          data={[
+            { title: 'Losses', value: team.totalMatches - team.totalWins, color: '#a34d5d' },
+            { title: 'Wins', value: team.totalWins, color: '#4da375' },
+          ]}
+        />
+      </div>
+      <div className="match-detail-section">
+        <h3>Latest Matches</h3>
+        <MatchDetailCard teamName={team.teamName} match={team.matches[0]}></MatchDetailCard>
+      </div>
       {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName} match={match}></MatchSmallCard>)}
+      <div className="more-link">
+        <a href="#">More ></a>
+      </div>
     </div>
   );
 }
