26832ed386f1833296d8ce35aeb6a1f2e4c0b304
JayramNanoskar <jayram.n978@gmail.com>
2021-07-22 14:15:53 +0530

    Adding backend REST API

--- src/main/java/com/jsn/ipldashboard/repository/MatchRepository.java
+++ src/main/java/com/jsn/ipldashboard/repository/MatchRepository.java
@@ -1,12 +1,15 @@
 package com.jsn.ipldashboard.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import com.jsn.ipldashboard.model.Match;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface MatchRepository extends CrudRepository<Match, Long>{ //Repository to get Match information
     
@@ -16,4 +19,8 @@ public interface MatchRepository extends CrudRepository<Match, Long>{ //Reposito
         return getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, PageRequest.of(0, count));
     }
 
+    // List<Match> getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(String teamName1, LocalDate date1, LocalDate date2, String teamName2, LocalDate date3, LocalDate date4); //Gives all matches for given team for given particular year
+
+    @Query("select m from Match m where (m.team1 = :teamName or m.team2 = :teamName) and m.date between :dateStart and :dateEnd order by date desc")
+    List<Match> getMatchesByTeamBetweenDates(@Param("teamName") String teamName, @Param("dateStart") LocalDate dateStart, @Param("dateEnd") LocalDate dataEnd); //Gives all matches for given team for given particular year
 }
